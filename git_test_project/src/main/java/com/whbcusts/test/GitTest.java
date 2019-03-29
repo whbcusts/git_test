c96dfddf7b525120450a799b42cc4a89f19573c6
whbcusts <whbcusts@gmail.com>
2019-03-29 17:18:26 +0800

    建立一个GitTest类

---
+++ git_test_project/src/main/java/com/whbcusts/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.whbcusts.test;
+
+/**
+ * @author Administrator
+ * @date 2019.3.29 17:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("石原里美");
+        System.out.println("石原里美");
+        System.out.println("石原里美");
+        System.out.println("石原里美");
+    }
+}
